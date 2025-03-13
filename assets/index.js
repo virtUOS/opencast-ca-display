6640593df06b76d664eff75ec23147b2c89823a9
janni06 <janni.wielpuetz@gmail.com>
2025-03-13 14:52:04 +0100

    Show no network connection

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,10 @@
 var config = null;
-updateCalendar();
 var is_active = null;
 var calendar = [];
+var network_status = {
+	interfaces: [],
+	connected: false,
+};
 
 /**
  * Load configuration and initialize timer
@@ -11,7 +14,10 @@ fetch("/config")
 	.then((cfg) => {
 		config = cfg;
 		console.info("config", config);
+		updateNetworkStatus();
 		setInterval(updateTimer, 1000);
+		setInterval(updateNetworkStatus, 1000);
+		updateCalendar();
 		setInterval(updateCalendar, 60000);
 	});
 
@@ -29,48 +35,56 @@ function updateView(active) {
 
 	// Update logo
 	document.getElementById("logo").src = active.image.replace(/\s/g, "");
-
-	document.getElementById("info").innerText = parseCalendar(active);
+	if (network_status.connected) {
+		document.getElementById("info").innerText = parseCalendar(active);
+	}
 }
 
 /**
  * Check for capture agent status
  */
 function updateTimer() {
-	fetch("/status")
-		.then((response) => {
-			if (!response.ok) {
-				const active = config.unknown;
-				active.text = response.statusText;
-				updateView(active);
-				throw Error(response.statusText);
-			}
-			return response.json();
-		})
-		.then((capturing) => {
-			console.debug("capturing", capturing);
-			is_active = capturing;
-			updateView(capturing ? config.capturing : config.idle);
-		});
+	if (network_status.connected == true) {
+		fetch("/status")
+			.then((response) => {
+				if (!response.ok) {
+					const active = config.unknown;
+					active.text = response.statusText;
+					updateView(active);
+					throw Error(response.statusText);
+				}
+				return response.json();
+			})
+			.then((capturing) => {
+				console.debug("capturing", capturing);
+				is_active = capturing;
+				updateView(capturing ? config.capturing : config.idle);
+			});
+	} else {
+		console.log("Network status is not connected");
+		const active = config.unknown;
+		active.text = "No network connection";
+		updateView(active);
+	}
 }
 
 function parseCalendar(active) {
-	let time_remaining = 0;
 	console.debug("Is Active? ", is_active);
-	const event_time = is_active ? calendar[0].end : calendar[0].start;
-
-	now = Date.now();
-	if (calendar.length > 0) {
-		// TODO Maybe switch 'is_active' to 'capturing' here?
-		//t = is_active ? calendar[0].End : calendar[0].Start;
-		time_remaining = event_time - now;
-		console.debug("Time Remaining: ", time_remaining, event_time, now);
-	} else {
+	if (calendar.length == 0) {
 		console.debug("Calendar is empty");
 		if (!is_active) {
 			return active.none;
 		}
 	}
+	let time_remaining = 0;
+
+	const event_time = is_active ? calendar[0].end : calendar[0].start;
+
+	now = Date.now();
+	// TODO Maybe switch 'is_active' to 'capturing' here?
+	//t = is_active ? calendar[0].End : calendar[0].Start;
+	time_remaining = event_time - now;
+	console.debug("Time Remaining: ", time_remaining, event_time, now);
 
 	hours =
 		time_remaining > 0 ? Math.floor(time_remaining / (1000 * 60 * 60)) : 0;
@@ -90,13 +104,31 @@ function parseCalendar(active) {
 }
 
 function updateCalendar() {
-	fetch("/calendar")
+	if (network_status.connected) {
+		fetch("/calendar")
+			.then((response) => {
+				console.debug("Calendar fetched; Status ", response.status);
+				return response.json();
+			})
+			.then((json) => {
+				console.log("Calendar ", json);
+				calendar = json;
+			});
+	}
+}
+
+function updateNetworkStatus() {
+	fetch("/network_info")
 		.then((response) => {
-			console.debug("Calendar fetched; Status ", response.status);
 			return response.json();
 		})
 		.then((json) => {
-			console.log("Calendar ", json);
-			calendar = json;
+			network_status = json;
+			if (!network_status.connected) {
+				console.log("No network");
+				const active = config.unknown;
+				active.text = "No network connection";
+				updateView(active);
+			}
 		});
 }
