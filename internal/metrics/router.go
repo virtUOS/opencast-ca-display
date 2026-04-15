d2308a97470820d5905903b047d96e95f8c236bb
janni06 <janni.wielpuetz@gmail.com>
2026-04-15 18:19:05 +0200

    Create metrics router

---
+++ internal/metrics/router.go
@@ -0,0 +1,21 @@
+package metrics
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func setupRouter() *gin.Engine {
+	router := gin.Default()
+
+	// disable all proxies
+	err := router.SetTrustedProxies(nil)
+	if err != nil {
+		log.Fatalf("Failed to set trusted proxies: %v", err)
+	}
+
+	router.GET("/metrics", gin.WrapH(promhttp.Handler()))
+	return router
+}
