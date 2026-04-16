8d47e4734c76b5a328dce066fc79a3283d714047
janni06 <janni.wielpuetz@gmail.com>
2026-04-16 18:46:10 +0200

    Add more fetching options

--- internal/opencast/api_event.go
+++ internal/opencast/api_event.go
@@ -1,6 +1,7 @@
 package opencast
 
 import (
+	"errors"
 	"fmt"
 	"net/url"
 	"time"
@@ -39,9 +40,10 @@ func (events) GetEventsByCaptureAgent(agentID string) ([]Event, error) {
 	return eventData, nil
 }
 
-func (events) GetUpcommingEvents(agentID string, duration time.Duration) ([]Event, error) {
-	start := time.Now()
-	end := start.Add(duration)
+func (events) GetEventsBeween(agentID string, start time.Time, end time.Time) ([]Event, error) {
+	if start.UnixMilli() > end.UnixMilli() {
+		return nil, errors.New("The start time can´t be after the end time.")
+	}
 
 	var eventData []Event
 
@@ -56,4 +58,67 @@ func (events) GetUpcommingEvents(agentID string, duration time.Duration) ([]Even
 	}
 
 	return eventData, nil
+}
+
+func (event events) GetUpcommingEvents(agentID string, duration time.Duration) ([]Event, error) {
+	start := time.Now()
+	end := start.Add(duration)
+
+	return event.GetEventsBeween(agentID, start, end)
+}
+
+func (event events) GetCurrentEvent(agentID string) (bool, Event, error) {
+	start := time.Now().Add(-time.Hour * 24) // Has to be set to another value because otherwise events that are curretnly running are not captured
+	end := time.Now().Add(time.Minute * 10)  // the maximum can be now because every started event has started by now (10 minutes are just for a buffer)
+
+	events, err := event.GetEventsBeween(agentID, start, end)
+	if err != nil {
+		return false, Event{}, err
+	}
+
+	for _, eventData := range events {
+		eventStart, err := time.Parse(time.RFC3339, eventData.Scheduling.Start)
+		if err != nil {
+			return false, Event{}, err
+		}
+		eventEnd, err := time.Parse(time.RFC3339, eventData.Scheduling.End)
+		if err != nil {
+			return false, Event{}, err
+		}
+
+		if eventStart.Unix() <= time.Now().Unix() && eventEnd.Unix() >= time.Now().Unix() {
+			// there can be only one event which is currently scheduled
+			return true, eventData, nil
+		}
+	}
+
+	return false, Event{}, nil
+}
+
+func (event events) GetNextEvent(agentID string) (bool, Event, error){
+	start := time.Now().Add(-time.Hour * 24) // Has to be set to another value because otherwise events that are curretnly running are not captured
+	end := time.Now().Add(time.Minute * 10)  // the maximum can be now because every started event has started by now (10 minutes are just for a buffer)
+
+	events, err := event.GetEventsBeween(agentID, start, end)
+	if err != nil {
+		return false, Event{}, err
+	}
+
+	for _, eventData := range events {
+		eventStart, err := time.Parse(time.RFC3339, eventData.Scheduling.Start)
+		if err != nil {
+			return false, Event{}, err
+		}
+		eventEnd, err := time.Parse(time.RFC3339, eventData.Scheduling.End)
+		if err != nil {
+			return false, Event{}, err
+		}
+
+		if eventStart.Unix() <= time.Now().Unix() && eventEnd.Unix() >= time.Now().Unix() {
+			// there can be only one event which is currently scheduled
+			return true, eventData, nil
+		}
+	}
+
+	return false, Event{}, nil
 }
