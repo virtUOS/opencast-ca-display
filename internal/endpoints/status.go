d47c9b98f20e38bf721e4928a9742c6a24e3e3db
janni06 <janni.wielpuetz@gmail.com>
2026-04-16 18:46:50 +0200

    Initial fallback handling for agent status

--- internal/endpoints/status.go
+++ internal/endpoints/status.go
@@ -18,17 +18,39 @@ type AgentStateResult struct {
 }
 
 func statusEndpoint(c *gin.Context) {
-	t, err := opencast.Agents.Get(localConfig.Opencast.Agent)
-	if err != nil {
+	agent, err := opencast.Agents.Get(localConfig.Opencast.Agent)
+	if err != nil { // TODO enhance the error handling to distinct between errors
 		slog.Error("Something went wrong")
 		c.JSON(http.StatusInternalServerError, nil)
 		metrics.UpdateState("internal_server_error")
 		return
 	}
 
+	// update metrics to show, that the displays content was updated the last time at the current time
 	metrics.UpdateTime()
 
-	metrics.UpdateState(t.Status)
+	// update the state shown in the metrics endpoint
+	metrics.UpdateState(agent.Status)
 
-	c.JSON(http.StatusOK, t.Status == "capturing")
+	// Return "true" if there is currently a recording happening and "false" otherwise
+	capturing := false
+	switch agent.Status {
+	case "capturing":
+		capturing = true
+	case "uploading":
+		// If the agent says, it´s uploading, there is the posibility that the agent is curretnly actually filming. 
+		// This edge case is handled here.
+		exists, _, err := opencast.Events.GetCurrentEvent(localConfig.Opencast.Agent)
+		if err != nil {
+			slog.Error("Something went wrong")
+			c.JSON(http.StatusInternalServerError, nil)
+			metrics.UpdateState("internal_server_error")
+			return
+		}
+		capturing = exists
+	default:
+		capturing = false
+	}
+
+	c.JSON(http.StatusOK, capturing)
 }
