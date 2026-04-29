098e62eb180ffadb6439f264d3f8de960f27304d
janni06 <janni.wielpuetz@gmail.com>
2026-04-29 15:59:46 +0200

    Add methods to retrieve current and next event from opencast calendar

--- internal/opencast/api_event.go
+++ internal/opencast/api_event.go
@@ -67,58 +67,83 @@ func (event events) GetUpcommingEvents(agentID string, duration time.Duration) (
 	return event.GetEventsBeween(agentID, start, end)
 }
 
-func (event events) GetCurrentEvent(agentID string) (bool, Event, error) {
-	start := time.Now().Add(-time.Hour * 24) // Has to be set to another value because otherwise events that are curretnly running are not captured
-	end := time.Now().Add(time.Minute * 10)  // the maximum can be now because every started event has started by now (10 minutes are just for a buffer)
+func (event events) GetCurrentEvent(agentID string) (*Event, error) {
+	start := time.Now().Add(-time.Hour * 24) // All future events can start at the earlist at the current time.
+	end := time.Now().Add(time.Minute * 10)  // Only events within the next 24 hours are considered
 
-	events, err := event.GetEventsBeween(agentID, start, end)
+	var eventsList []Event
+
+	query := url.Values{}
+
+	query.Add("withscheduling", "true")
+	query.Add("filter", fmt.Sprintf("agent_id:%s,start:%s/%s", agentID, start.UTC().Format(time.RFC3339), end.UTC().Format(time.RFC3339)))
+	query.Add("sort", "start_date:DESC")
+	query.Add("limit", "1")
+
+	err := opencastClient.GetJSONwithQuery("/api/events", query, &eventsList)
 	if err != nil {
-		return false, Event{}, err
+		return nil, err
 	}
 
-	for _, eventData := range events {
+	for _, eventData := range eventsList {
+		// fmt.Println(eventData)
 		eventStart, err := time.Parse(time.RFC3339, eventData.Scheduling.Start)
 		if err != nil {
-			return false, Event{}, err
+			println("error parsing the date")
+			return nil, err
 		}
 		eventEnd, err := time.Parse(time.RFC3339, eventData.Scheduling.End)
 		if err != nil {
-			return false, Event{}, err
+			println("error parsing the date")
+			return nil, err
 		}
 
 		if eventStart.Unix() <= time.Now().Unix() && eventEnd.Unix() >= time.Now().Unix() {
 			// there can be only one event which is currently scheduled
-			return true, eventData, nil
+			return &eventData, nil
 		}
 	}
 
-	return false, Event{}, nil
+	return nil, nil
 }
 
-func (event events) GetNextEvent(agentID string) (bool, Event, error){
-	start := time.Now().Add(-time.Hour * 24) // Has to be set to another value because otherwise events that are curretnly running are not captured
-	end := time.Now().Add(time.Minute * 10)  // the maximum can be now because every started event has started by now (10 minutes are just for a buffer)
+func (event events) GetNextEvent(agentID string) (*Event, error) {
+	start := time.Now()                    // All future events can start at the earlist at the current time.
+	end := time.Now().Add(time.Hour * 720) // Only events within the next 24 hours are considered
 
-	events, err := event.GetEventsBeween(agentID, start, end)
+	var eventsList []Event
+
+	query := url.Values{}
+
+	query.Add("withscheduling", "true")
+	query.Add("filter", fmt.Sprintf("agent_id:%s,start:%s/%s", agentID, start.UTC().Format(time.RFC3339), end.UTC().Format(time.RFC3339)))
+	query.Add("sort", "start_date:ASC")
+	query.Add("limit", "1")
+
+	err := opencastClient.GetJSONwithQuery("/api/events", query, &eventsList)
 	if err != nil {
-		return false, Event{}, err
+		return nil, err
 	}
 
-	for _, eventData := range events {
-		eventStart, err := time.Parse(time.RFC3339, eventData.Scheduling.Start)
-		if err != nil {
-			return false, Event{}, err
-		}
-		eventEnd, err := time.Parse(time.RFC3339, eventData.Scheduling.End)
-		if err != nil {
-			return false, Event{}, err
-		}
+	for _, eventData := range eventsList {
+		// fmt.Println(eventData)
+		// eventStart, err := time.Parse(time.RFC3339, eventData.Scheduling.Start)
+		// if err != nil {
+		// 	println("error parsing the date")
+		// 	return false, Event{}, err
+		// }
+		// eventEnd, err := time.Parse(time.RFC3339, eventData.Scheduling.End)
+		// if err != nil {
+		// 	println("error parsing the date")
+		// 	return false, Event{}, err
+		// }
 
-		if eventStart.Unix() <= time.Now().Unix() && eventEnd.Unix() >= time.Now().Unix() {
-			// there can be only one event which is currently scheduled
-			return true, eventData, nil
-		}
+		// if eventStart.Unix() <= time.Now().Unix() && eventEnd.Unix() >= time.Now().Unix() {
+		// 	// there can be only one event which is currently scheduled
+		// 	return true, eventData, nil
+		// }
+		return &eventData, nil
 	}
 
-	return false, Event{}, nil
+	return nil, nil
 }
